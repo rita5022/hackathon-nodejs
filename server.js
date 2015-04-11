686b8f511a11fd57ca3769f5de9733ec44856420
Rita Su <rita5022@gmail.com>
2015-04-11 08:20:40 +0000

    can send private message to specific person

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var socketio = require('socket.io');
 //建立一個新的 socket.io server
 var io = socketio(8080);//port
 
-//建立一個userlist
+//建立一個userlist物件
 var userList = {};
+
+//紀錄username和socket id 對應
+var user_match = {};
 
 //每條新的連線近來都要做事件綁定
 io.on('connection',function(socket){
@@ -13,12 +16,14 @@ io.on('connection',function(socket){
   //使用者上線通知
   socket.on('user_in',function(nickname){
 	  userList[socket.id] = nickname;
+	  user_match[socket.id]=socket;
 	  io.emit('user_list',userList);
   });
   
   //使用者離線通知
   socket.on('disconnect',function(){
 	  delete userList[socket.id];
+	  delete user_match[socket.id];
 	  io.emit('user_list',userList);
   });
 	
@@ -26,6 +31,14 @@ io.on('connection',function(socket){
   socket.on('message',function(form,msg){
 	  io.emit('message',form,msg);
   });
+  
+  //私訊給特定人
+  socket.on('private message',function(form, sendToId, msg){
+    //io.emit io是給全體
+    user_match[sendToId].emit('private message', form, msg);
+  
+  });
+  
 });
 
 /*setTimeout(
